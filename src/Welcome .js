9ae939a2696c22fa7e7b9d953ab8c60c132e2445
Cristian <cristianrimbu@gmail.com>
2022-07-12 17:15:40 +0200

    es conditional-rendering-02

--- src/Welcome .js
+++ src/Welcome .js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Age } from './Age';
 
+/* Modify the Welcome component so that the Age component is rendered 
+only if the age prop is present. */
 export class Welcome extends React.Component {
   render() {
     return (
       <div>
         <p>Welcome, {this.props.name ?? 'Caio'}</p>
-        {/* <Age age={this.props.age>18 ? this.props.age: "< 18"} /> */}
-        <Age age={this.props.age > 18 && this.props.age} />
-        {/* stampa solo se >18 */}
+        {this.props.age && <Age age={this.props.age > 18 && this.props.age} />}
       </div>
     );
   }
 }
-// age passa da App a Welcome e da Welcome a Age
