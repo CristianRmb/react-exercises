a4e81cc28beba2854c21c228d8867c5a96efa285
Cristian <cristianrimbu@gmail.com>
2022-07-12 12:59:29 +0200

    es components-02

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom';
+import { HelloWorld } from './HelloWorld';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<HelloWorld/>, root);
