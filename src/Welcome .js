1c0f35f9071d628ae47125e14a94c0a352f07aa9
Cristian <cristianrimbu@gmail.com>
2022-07-12 17:47:58 +0200

    conditional-rendering-05

--- src/Welcome .js
+++ src/Welcome .js
@@ -8,9 +8,10 @@ export class Welcome extends React.Component {
     return (
       <div>
         <p>Welcome, {this.props.name ?? 'Caio'}</p>
-        {this.props.age > 18 &&
+        {/* {this.props.age > 18 &&
           this.props.age < 65 &&
-          this.props.name === 'John' && <Age age={this.props.age} />}
+          this.props.name === 'John' && <Age age={this.props.age} />} */}
+        <Age age={this.props.age} />
       </div>
     );
   }
