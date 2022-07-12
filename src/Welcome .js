0b4d8978e99e8b823dfb2c82afeec7a137c03e34
Cristian <cristianrimbu@gmail.com>
2022-07-12 16:16:07 +0200

    es props-05

--- src/Welcome .js
+++ src/Welcome .js
@@ -1,20 +1,14 @@
 import React from 'react';
-
-/* Create a Welcome class component that receives a name prop and renders the Welcome, 
-{name}! message within a p tag. Render this component to you App component, 
-passing it a name prop of your choosing. */
+import { Age } from './Age';
 
 export class Welcome extends React.Component {
   render() {
     return (
       <div>
         <p>Welcome, {this.props.name ?? 'Caio'}</p>
-        <p>Your age is {this.props.age ?? '000'}</p>
+        <Age age={this.props.age} />
       </div>
     );
   }
 }
-/* Anche questa è una possibilita:  */
-// Welcome.defaultProps = {
-//   name: 'Tizio',
-// };
+// age passa da App a Welcome e da Welcome a Age
