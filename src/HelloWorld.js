556ec1e2d7bafdf2c72a67a9131cbd76e6a4e769
Cristian <cristianrimbu@gmail.com>
2022-07-12 17:36:18 +0200

    es conditional-rendering-04

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,6 +1,5 @@
 import React from 'react'
 
-/* Write a `Hello` class components that renders the `"Hello, World!"` message within an `h1` tag. */
 export class HelloWorld extends React.Component{
   render(){
     return (
@@ -8,5 +7,3 @@ export class HelloWorld extends React.Component{
     );
   }
 }
-/* Modify the `Hello` component so that it shows a `p` tag below the `h1` tag with the message 
-`"What a beautiful day!"`. */
