152722d15b38f2cb3f7d569b587c30cb6817e07d
Cristian <cristianrimbu@gmail.com>
2022-07-15 08:25:21 +0200

    fix: es state-04

--- src/CounterDisplay.js
+++ src/CounterDisplay.js
@@ -4,21 +4,8 @@ import React from 'react';
 and render it within the Counter component, passing it the count state variable as a prop. */
 
 export class CounterDisplay extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      count: this.props.initialValue,
-    };
-
-    setInterval(() => {
-      this.setState(() => {
-        return { count: this.state.count + this.props.incrementAmount };
-      });
-    }, this.props.incrementInterval);
-  }
 
   render() {
-    return <h1>Count: {this.state.count}</h1>;
+    return <h1>Count: {this.props.count}</h1>;
   }
 }
