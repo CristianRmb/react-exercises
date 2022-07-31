6b8c4bf3b3abb4e733a268d18308abff655a0f55
Cristian <cristianrimbu@gmail.com>
2022-07-31 14:16:01 +0200

    es context-02

--- src/DisplayLanguage.js
+++ src/DisplayLanguage.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { LanguageContext } from './LanguageContext';
 
-/* Create a LanguageContext and wrap a DisplayLanguage component within its provider.
-Add a select tag to the component containing the DisplayLanguage component that allows 
-the user to select the language, and pass it as a value to the Provider. */
+/* Consume the LanguageContext within the DisplayLanguage component by using the context consumer, 
+and show the selected language in an h1 tag. */
 
+const Language = {
+  en: 'English',
+  it: 'Italiano',
+};
 export class DisplayLanguage extends React.Component {
   render() {
     return (
       <div>
+        <LanguageContext.Consumer>
+          {(language) => {
+            return <h1>Language: {Language[language]}</h1>;
+          }}
+        </LanguageContext.Consumer>
       </div>
     );
   }
