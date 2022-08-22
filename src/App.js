3b719a2ae80f814f27132e9731b751d9c067fe0d
Cristian <cristianrimbu@gmail.com>
2022-08-22 10:08:47 +0200

    es react-router-04

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
-import { useState } from 'react';
 import { Welcome } from './Welcome ';
 import { Container } from './Container';
-import { LanguageContext } from './LanguageContext';
-import { HookCounter } from './HookCounter';
-import { FilteredList } from './FilteredList';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import { Counter } from './Counter';
 import { ShowGithubUser } from './ShowGithubUser';
 
 export function App() {
-  const [language, setLanguage] = useState('en');
-
-  function handleChangeLanguage(event) {
-    setLanguage(event.target.value);
-  }
 
   return (
     <div>
-      <select value={language} onChange={handleChangeLanguage}>
-        <option value='en'>English</option>
-        <option value='it'>Italiano</option>
-      </select>
-      <LanguageContext.Provider value={language}>
         <Container title={'React Exercises'}>
-          <Welcome age={27} />
-          <HookCounter />
-          <FilteredList />
-        </Container>
-      </LanguageContext.Provider>
-      <Container>
+        <Link className='mx-1' to={'/'}>Home</Link>
+        <Link className='mx-1' to={'/counter'}>Counter</Link>
+        <Link className='mx-1' to={'/users/CristianRmb'}>User</Link>
         <Routes>
           <Route path='/' element={<Welcome name={'CRISTIAN'} />} />
           <Route path='/counter' element={<Counter />} />
