0524c1eb85174e24ffe7c5fc7a8ab790e85e7ea1
Cristian <cristianrimbu@gmail.com>
2022-08-22 10:16:48 +0200

    es react-router-05

--- src/App.js
+++ src/App.js
@@ -5,17 +5,31 @@ import { Counter } from './Counter';
 import { ShowGithubUser } from './ShowGithubUser';
 
 export function App() {
-
   return (
     <div>
-        <Container title={'React Exercises'}>
-        <Link className='mx-1' to={'/'}>Home</Link>
-        <Link className='mx-1' to={'/counter'}>Counter</Link>
-        <Link className='mx-1' to={'/users/CristianRmb'}>User</Link>
+      <Container title={'React Exercises'}>
+        <Link className='mx-1' to={'/'}>
+          Home
+        </Link>
+        <Link className='mx-1' to={'/counter'}>
+          Counter
+        </Link>
+        <Link className='mx-1' to={'/users/CristianRmb'}>
+          User
+        </Link>
+        <Link to={`/${Math.random()}`}>Random path</Link>
         <Routes>
           <Route path='/' element={<Welcome name={'CRISTIAN'} />} />
           <Route path='/counter' element={<Counter />} />
           <Route path='users/:username' element={<ShowGithubUser />} />
+          <Route
+            path='*'
+            element={
+              <div>
+                <p>Not found</p>
+              </div>
+            }
+          />
         </Routes>
       </Container>
     </div>
