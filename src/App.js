497faded431e37a352dcf4bff286e39fde9039b6
Cristian <cristianrimbu@gmail.com>
2022-08-22 10:51:07 +0200

    es react-router-06

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import { Container } from './Container';
 import { Routes, Route, Link } from 'react-router-dom';
 import { Counter } from './Counter';
 import { ShowGithubUser } from './ShowGithubUser';
+import { GithubUserList } from './GithubUserList';
+
+/* Add a Route to the users path that shows the GihubUserList component from useEffect 04. 
+Modify it so that instead of showing the GithubUser component for each username entered, 
+it shows a link to a nested route that shows the ShowGithubUser component. In doing so, 
+remove the /users/:username route from the App component, and add a new nested route within the /users route. */
 
 export function App() {
   return (
@@ -14,14 +20,16 @@ export function App() {
         <Link className='mx-1' to={'/counter'}>
           Counter
         </Link>
-        <Link className='mx-1' to={'/users/CristianRmb'}>
+        <Link className='mx-1' to={'/users'}>
           User
         </Link>
         <Link to={`/${Math.random()}`}>Random path</Link>
         <Routes>
           <Route path='/' element={<Welcome name={'CRISTIAN'} />} />
+          <Route path='users' element={<GithubUserList />}>
+            <Route path=':username' element={<ShowGithubUser />} />
+          </Route>
           <Route path='/counter' element={<Counter />} />
-          <Route path='users/:username' element={<ShowGithubUser />} />
           <Route
             path='*'
             element={
--- src/GithubUserList.js
+++ src/GithubUserList.js
@@ -1,13 +1,13 @@
 /* Create a GithubUserList component that maintains an array of usernames, showing a GithubUser component 
 for each username entered. The usernames should be added to the array using an input field and a button. */
 
-import React, { useState, useEffect } from 'react';
-import { GithubUser } from './GithubUser';
+import React, { useState } from 'react';
+import { Link, Outlet } from 'react-router-dom';
 
 export function GithubUserList() {
   const [arrayUsername, setArrayUsername] = useState([]);
   const [inputValue, setInputValue] = useState();
-  
+
   function HandleInput(event) {
     setInputValue(event.target.value);
   }
@@ -19,15 +19,8 @@ export function GithubUserList() {
 
   return (
     <div>
-      <input name='username' value={inputValue} onChange={HandleInput} />
-      <button onClick={AddUsername}>Add Username</button>
-      <ul>
-        {arrayUsername.map((name, index) => (
-          <div key={name + index}>
-            <GithubUser username={name} />
-          </div>
-        ))}
-      </ul>
+      <Link to={'/users/CristianRmb'}>Show User</Link>
+      <Outlet />
     </div>
   );
 }
